50cac48a831788dc1d1d8e4e7c99c517a218e060
Harshith287 <95353747+Harshith287@users.noreply.github.com>
2026-01-22 12:54:44 +0530

    Add new theme 'ginkgo-gold' to themes.ts (#1436)
    Signed-off-by: Harshith287 <95353747+Harshith287@users.noreply.github.com>

--- features/Preferences/data/themes.ts
+++ features/Preferences/data/themes.ts
@@ -267,6 +267,12 @@ const baseThemeSets: BaseThemeGroup[] = [
         secondaryColor: 'oklch(70.0% 0.075 55.0 / 1)'
       },
       {
+        id: 'ginkgo-gold',
+        backgroundColor: 'oklch(23.0% 0.045 85.0 / 1)',
+        mainColor: 'oklch(88.0% 0.170 95.0 / 1)',
+        secondaryColor: 'oklch(72.0% 0.135 75.0 / 1)'
+      },
+      {
         id: 'shaved-ice',
         backgroundColor: 'oklch(95.0% 0.025 215.0 / 1)',
         mainColor: 'oklch(60.0% 0.195 25.0 / 1)',
